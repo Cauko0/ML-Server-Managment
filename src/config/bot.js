0c05db1abfedaa174460a780f24525dcdb2300a1
Cauko0 <samuelcerny164@gmail.com>
2026-06-13 13:06:27 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "ML Server Manager on duty",
         // Activity type number (0 = Playing).
         type: 0, 
       },
